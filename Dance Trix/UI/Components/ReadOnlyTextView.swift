59d7897efa3628cab95b915aede70329f276b201
Daniel Palmer <d.palmer101@googlemail.com>
2017-09-25 23:00:24 +0100

    Added fade-out edges on ReadOnlyTextView

--- Dance Trix/UI/Components/ReadOnlyTextView.swift
+++ Dance Trix/UI/Components/ReadOnlyTextView.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ReadOnlyTextView: UITextView {
 
+    let fadePercentage = CGFloat(0.2)
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         self.isEditable = false
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let transparent = UIColor.clear.cgColor
+        let opaque = Theme.colorBackground.cgColor
+        
+        let maskLayer = CALayer()
+        maskLayer.frame = self.bounds
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: self.bounds.origin.x,
+                                     y: 0,
+                                     width: self.bounds.size.width,
+                                     height: self.bounds.size.height)
+        gradientLayer.colors = [transparent, opaque, opaque, transparent]
+        gradientLayer.locations = [0, 0.2, 0.8, 1]
+        
+        maskLayer.addSublayer(gradientLayer)
+        self.layer.mask = maskLayer
     }
     
     override func didMoveToWindow() {
