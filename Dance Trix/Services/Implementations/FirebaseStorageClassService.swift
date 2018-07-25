6954c2f5381ba11349df42001c9a3070d3ab1d3a
Daniel Palmer <d.palmer101@googlemail.com>
2018-07-25 22:56:29 +0100

    [0.0.5 (build #119)] Removed forced unwrapping of optionals in case of bad file data, and added backup file encoding so files can be UTF-8 or ANSII

--- Dance Trix/Services/Implementations/FirebaseStorageClassService.swift
+++ Dance Trix/Services/Implementations/FirebaseStorageClassService.swift
@@ -43,29 +43,33 @@ class FirebaseStorageClassService : ClassServiceType {
     func getClassDates(_ classDetails: Class,
                        successHandler: @escaping ([DateInterval]) -> Void,
                        errorHandler: @escaping (Error) -> Void) {
-        let classesMenuCsv = Storage.storage().reference().child(classDetails.datesLocation)
+        let classDatesCsv = Storage.storage().reference().child(classDetails.datesLocation)
         
         log.info("Retrieving dates for \(classDetails.name)...")
         
         log.debug("    Downloading CSV from Firebase storage: \(classDetails.datesLocation)")
         
         // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
-        classesMenuCsv.getData(maxSize: 1 * 1024 * 1024) { data, error in
+        classDatesCsv.getData(maxSize: 1 * 1024 * 1024) { data, error in
             if let error = error {
                 log.warning(["    An error occurred downloading the CSV file: \(classDetails.datesLocation)", error])
                 errorHandler(error)
-            } else {
+            } else if let data = data {
                 log.debug("    Downloaded CSV from Firebase storage: \(classDetails.datesLocation)")
                 
-                let csvString = String(data: data!, encoding: String.Encoding.utf8)
-                
-                log.debug("    Parsing CSV: \(classDetails.datesLocation)")
-                let dates = ClassDatesParser.parse(csvString: csvString!)
-                log.debug("    Parsed CSV: \(classDetails.datesLocation)")
-                
-                log.info("...retrieved dates for \(classDetails.name)")
-                
-                successHandler(dates)
+                if let csvString = self.dataToString(data: data) {
+                    log.debug("    Parsing CSV: \(classDetails.datesLocation)")
+                    let dates = ClassDatesParser.parse(csvString: csvString)
+                    log.debug("    Parsed CSV: \(classDetails.datesLocation)")
+                    
+                    log.info("...retrieved dates for \(classDetails.name)")
+                    
+                    successHandler(dates)
+                } else {
+                    errorHandler(ClassesError.noClassDates(classDetails: classDetails))
+                }
+            } else {
+                errorHandler(ClassesError.noClassDates(classDetails: classDetails))
             }
         }
     }
@@ -73,23 +77,27 @@ class FirebaseStorageClassService : ClassServiceType {
     func getClassDescription(_ classDetails: Class,
                              successHandler: @escaping (String) -> Void,
                              errorHandler: @escaping (Error) -> Void) {
-        let classesMenuCsv = Storage.storage().reference().child(classDetails.descriptionLocation)
+        let classDescriptionFile = Storage.storage().reference().child(classDetails.descriptionLocation)
         
         log.info("Retrieving description for \(classDetails.name)...")
         
         log.debug("    Downloading text from Firebase storage: \(classDetails.descriptionLocation)")
         
         // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
-        classesMenuCsv.getData(maxSize: 1 * 1024 * 1024) { data, error in
+        classDescriptionFile.getData(maxSize: 1 * 1024 * 1024) { data, error in
             if let error = error {
                 log.warning(["    An error occurred downloading the text file: \(classDetails.descriptionLocation)", error])
                 errorHandler(error)
-            } else {
+            } else if let data = data {
                 log.debug("    Downloaded text from Firebase storage: \(classDetails.descriptionLocation)")
                 
-                let descriptionText = String(data: data!, encoding: String.Encoding.utf8)
-                
-                successHandler(descriptionText!)
+                if let descriptionText = self.dataToString(data: data) {
+                    successHandler(descriptionText)
+                } else {
+                    errorHandler(ClassesError.noClassDescription(classDetails: classDetails))
+                }
+            } else {
+                errorHandler(ClassesError.noClassDescription(classDetails: classDetails))
             }
         }
     }
@@ -108,19 +116,33 @@ class FirebaseStorageClassService : ClassServiceType {
             if let error = error {
                 log.warning(["    An error occurred downloading the CSV file: \(importantDatesCsv)", error])
                 errorHandler(error)
-            } else {
+            } else if let data = data {
                 log.debug("    Downloaded CSV from Firebase storage: \(importantDatesCsv)")
                 
-                let csvString = String(data: data!, encoding: String.Encoding.utf8)
-                
-                log.debug("    Parsing CSV: \(importantDatesCsv)")
-                let dates = ImportantDatesParser.parse(csvString: csvString!)
-                log.debug("    Parsed CSV: \(importantDatesCsv)")
-                
-                log.info("...retrieved important dates")
-                
-                successHandler(dates)
+                if let csvString = self.dataToString(data: data) {
+                    log.debug("    Parsing CSV: \(importantDatesCsv)")
+                    let dates = ImportantDatesParser.parse(csvString: csvString)
+                    log.debug("    Parsed CSV: \(importantDatesCsv)")
+                    
+                    log.info("...retrieved important dates")
+                    
+                    successHandler(dates)
+                } else {
+                    errorHandler(ClassesError.noImportantDates)
+                }
+            } else {
+                errorHandler(ClassesError.noImportantDates)
             }
+        }
+    }
+    
+    private func dataToString(data: Data) -> String? {
+        if let utf8String = String(data: data, encoding: String.Encoding.utf8) {
+            return utf8String
+        } else if let cp1252String = String(data: data, encoding: String.Encoding.windowsCP1252) {
+            return cp1252String
+        } else {
+            return nil
         }
     }
     
